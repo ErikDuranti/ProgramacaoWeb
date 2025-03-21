9debbdb2e3595f1c6b3cfd9fa418448338160101
ErikDuranti <erik.duranti@gmail.com>
2025-03-21 09:54:15 -0300

    Testando algumas arrays

--- aula02.php
+++ aula02.php
@@ -1,4 +1,14 @@
 <?php
 $lista = array("laranja", "maçã", "banana", "uva", "melancia");
 
+print_r($lista);
+echo "<br>";
+
+echo $lista[1], " - ";
+echo $lista[3];
+
+$pedro = array("nome" => "Pedro",
+               "idade" => 25, 
+               "cidade" => "São Paulo");
+echo "<br>";
 ?>
